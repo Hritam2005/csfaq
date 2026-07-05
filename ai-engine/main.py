0e0b4a3f873a36f6de70e26f12f03498f9a960cc
Soap <dudelol402@gmail.com>
2026-07-06 02:29:02 +0530

    chatbot working

--- ai-engine/main.py
+++ ai-engine/main.py
@@ -24,10 +24,10 @@ generation_config = {
   "temperature": 0.2,
   "top_p": 0.95,
   "top_k": 40,
-  "max_output_tokens": 1024,
+  "max_output_tokens": 2048,
 }
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash", 
+    model_name="gemini-2.5-flash", 
     generation_config=generation_config
 )
 
